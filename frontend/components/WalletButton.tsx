6dba59678c96d45a72ebb3ac4faab12fd1828891
wswsyy <shiyu689@qq.com>
2025-11-03 10:53:00 -0800

    test(frontend): make changes

---
+++ frontend/components/WalletButton.tsx
@@ -0,0 +1,17 @@
+﻿"use client";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+// Wallet connection button component
+export function WalletButton() {
+  return (
+    <ConnectButton
+      showBalance={false}
+      accountStatus={{
+        largeScreen: "address",
+        smallScreen: "avatar",
+      }}
+    />
+  );
+}
+
