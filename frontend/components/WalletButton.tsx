902131d0c23ffbab27885ee55578b83b4f8bdbf9
wswsyy <shiyu689@qq.com>
2025-11-06 12:56:00 -0800

    feat(frontend): make changes

--- frontend/components/WalletButton.tsx
+++ frontend/components/WalletButton.tsx
@@ -4,6 +4,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 // 2025-11-16 12:33 - Code update
 
 // Wallet connection button component
+// 2025-11-16 12:34 - Code update
 export function WalletButton() {
   return (
     <ConnectButton
